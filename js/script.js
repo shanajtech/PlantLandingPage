b1217d0fff3b0197ece7ac6f0f64ee0e1fa1ca61
Shanaj-akter <shanajj764@gmail.com>
2025-09-03 01:29:28 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(function(){
+// bannerslider start
+$('.bannerimgoverlay').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: true,
+  });
+// bannerslider end
+// bannerslider start
+// bannerslider end
+// bannerslider start
+// bannerslider end
+// bannerslider start
+// bannerslider end
+// bannerslider start
+// bannerslider end
+// bannerslider start
+// bannerslider end
+// bannerslider start
+// bannerslider end
+// bannerslider start
+// bannerslider end
+// bannerslider start
+// bannerslider end
+// bannerslider start
+// bannerslider end
+// bannerslider start
+// bannerslider end
+// bannerslider start
+// bannerslider end
+// bannerslider start
+// bannerslider end
+// bannerslider start
+// bannerslider end
+// bannerslider start
+// bannerslider end
+})
